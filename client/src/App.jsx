e6defc5ed18da4ec8e84d99c2581d52a2337e31e
ManhTTFU <tuanmanh97x@gmail.com>
2023-06-28 17:07:11 +0700

    set layout reactjs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,17 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+import Layout from './Layout'
 
 function App() {
     return (
         <>
             <Routes>
-                <Route index element={<IndexPage />}></Route>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<IndexPage />}></Route>
+                    <Route path="/login" element={<LoginPage />} />
+                </Route>
             </Routes>
         </>
     )
